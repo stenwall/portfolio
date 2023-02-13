7ec5df1b5c35772f3e952d86c840710936bd7b3e
Karin <karin.stenwall@gmail.com>
2023-02-13 23:28:13 +0100

    feat: add next/fonts ass css vars

--- components/layout/FlickLayout.tsx
+++ components/layout/FlickLayout.tsx
@@ -2,12 +2,18 @@ import React, { ReactNode } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Nunito_Sans } from '@next/font/google';
+import { Nunito_Sans, Nunito } from '@next/font/google';
 import styles from '@components/layout/Layout.module.scss';
 
 const nunitoSans = Nunito_Sans({
-  weight: ['200', '300', '400'],
-  style: ['normal'],
+  weight: ['200', '300', '400', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+});
+
+const nunito = Nunito({
+  weight: ['200', '300', '400', '600', '700'],
+  style: ['normal', 'italic'],
   subsets: ['latin']
 });
 
@@ -23,48 +29,52 @@ const FlickLayout = ({
   const { asPath: currentPath } = useRouter();
 
   return (
-  <>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <style jsx global>{`
-      html {
-        font-family: ${nunitoSans.style.fontFamily};
-        letter-spacing: 0.04em;
-      }
-      body {
-        background-color: #222222;
-        color: #dadada;
-      }
-    `}</style>
-    <header className={styles.header}>
-          <h1>Karin Stenwall / Director of Photography</h1>
-          <nav className={styles.nav}>
-            <Link
-              href="/dop"
-              className={currentPath === '/dop' ? styles.active : ''}
-            >
-              selected work
-            </Link>
-            <Link
-              href="/dop/resume"
-              className={currentPath === '/dop/resume' ? styles.active : ''}
-            >
-              resumé
-            </Link>
-            <Link
-              href="/dop/contact"
-              className={currentPath === '/dop/contact' ? styles.active : ''}
-            >
-              contact
-            </Link>
-          </nav>
-        </header>
-    {children}
-  </>
-);
-    }
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <style jsx global>{`
+        :root {
+          --nunito-sans: ${nunitoSans.style.fontFamily};
+          --nunito: ${nunito.style.fontFamily};
+        }
+        html {
+          font-family: var(--nunito-sans);
+          letter-spacing: 0.04em;
+        }
+        body {
+          background-color: #222222;
+          color: #dadada;
+        }
+      `}</style>
+      <header className={styles.header}>
+        <h1>Karin Stenwall / Director of Photography</h1>
+        <nav className={styles.nav}>
+          <Link
+            href="/dop"
+            className={currentPath === '/dop' ? styles.active : ''}
+          >
+            selected work
+          </Link>
+          <Link
+            href="/dop/resume"
+            className={currentPath === '/dop/resume' ? styles.active : ''}
+          >
+            resumé
+          </Link>
+          <Link
+            href="/dop/contact"
+            className={currentPath === '/dop/contact' ? styles.active : ''}
+          >
+            contact
+          </Link>
+        </nav>
+      </header>
+      {children}
+    </>
+  );
+};
 
 export default FlickLayout;
