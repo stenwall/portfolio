643c00ca9d2d5b538e65b5ddfc816066ad9e8990
Karin <karin.stenwall@gmail.com>
2023-02-15 13:03:07 +0100

    feat: jamila, hemmavid

--- pages/dop/hemmavid.tsx
+++ pages/dop/hemmavid.tsx
@@ -1,24 +1,51 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@styles/Flick.module.scss';
 import Layout from '@components/layout/FlickLayout';
+import hemmavid from 'assets/images/flick/projects/hemmavid-poster.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Hemmavid = () => (
   <Layout>
     <main className={styles.flick}>
       <h2>Hemmavid</h2>
-      <h3>Education</h3>
-      <ul>
-        <li>
-          2012-2015 Bachelor in cinematography at Stockholm academy of dramatic
-          arts, Sweden
-        </li>
-        <li>2006-2008 Basic filmproduction at Stockholm filmskola, Sweden</li>
-      </ul>
-      <h3>Music promos</h3>
-      <p>
-        Peter Bjorn & John, Existensminimum, Lisa Ekdahl, EP’s Trailerpark,
-        Soles of men, San Fairy Ann, APA
-      </p>
+      <section className={styles.film}>
+        <figure className={styles['profile-pic']}>
+          <Image
+            src={hemmavid}
+            alt="Poster for the film."
+            placeholder="blur"
+            fill
+          />
+        </figure>
+        <div className={styles['film-desc']}>
+          <ul>
+            <li>
+              <span>Director</span> Rebecka Ekholm
+            </li>
+            <li>
+              <span>Producer</span> Claes Hedlund/SKH
+            </li>
+            <li>
+              <span>12 min</span>
+            </li>
+          </ul>
+          <div>
+            <p>
+              Hemmavid is a documentary poem about three women in a small
+              village, Vormsele, in the interior of Västerbotten. We meet
+              Isabelle, who has returned home after a personal trauma, Rut who
+              has lived in the village all her life and Katharina - a German
+              woman who together with her British husband drove through the
+              village 8 years ago, and has stayed there ever since.
+            </p>
+            <p>
+              Hemmavid is about these people's relationship to a home, and a
+              place.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   </Layout>
 );
--- pages/dop/jamila.tsx
+++ pages/dop/jamila.tsx
@@ -1,24 +1,55 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@styles/Flick.module.scss';
 import Layout from '@components/layout/FlickLayout';
+import jamila from 'assets/images/flick/projects/jamila-poster.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Jamila = () => (
   <Layout>
     <main className={styles.flick}>
       <h2>Jamila</h2>
-      <h3>Education</h3>
-      <ul>
-        <li>
-          2012-2015 Bachelor in cinematography at Stockholm academy of dramatic
-          arts, Sweden
-        </li>
-        <li>2006-2008 Basic filmproduction at Stockholm filmskola, Sweden</li>
-      </ul>
-      <h3>Music promos</h3>
-      <p>
-        Peter Bjorn & John, Existensminimum, Lisa Ekdahl, EP’s Trailerpark,
-        Soles of men, San Fairy Ann, APA
-      </p>
+      <section className={styles.film}>
+        <figure className={styles['profile-pic']}>
+          <Image
+            src={jamila}
+            alt="Poster for the film."
+            placeholder="blur"
+            fill
+          />
+        </figure>
+        <div className={styles['film-desc']}>
+          <ul>
+            <li>
+              <span>Director</span> Sophie Vukovic
+            </li>
+            <li>
+              <span>Producer</span> Grand Slam Film
+            </li>
+            <li>
+              <span>13 min</span>
+            </li>
+          </ul>
+          <div>
+            <p>
+              <i>
+                She is on her way out when she sees the envelope. Jamila's
+                family has had their asylum application rejected. She pretends
+                not to have seen it, drags her little sister to school and meets
+                up with her friends. But suddenly, she sees them everywhere she
+                turns – the police.
+              </i>
+            </p>
+            <p className={styles.extra}>
+              –{' '}
+              <a className={styles.link} href="https://viddla.se/film/5647970">
+                Watch the film on Viddla{' '}
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   </Layout>
 );
