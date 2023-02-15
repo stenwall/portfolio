816400eb547157282335efd9f1d363d18b86cbef
Karin <karin.stenwall@gmail.com>
2023-02-15 12:30:08 +0100

    feat: yustina, bror

--- pages/dop/bror.tsx
+++ pages/dop/bror.tsx
@@ -1,24 +1,47 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@styles/Flick.module.scss';
 import Layout from '@components/layout/FlickLayout';
+import bror from 'assets/images/flick/projects/bror-poster.jpg';
 
 const Bror = () => (
   <Layout>
     <main className={styles.flick}>
       <h2>Bror</h2>
-      <h3>Education</h3>
-      <ul>
-        <li>
-          2012-2015 Bachelor in cinematography at Stockholm academy of dramatic
-          arts, Sweden
-        </li>
-        <li>2006-2008 Basic filmproduction at Stockholm filmskola, Sweden</li>
-      </ul>
-      <h3>Music promos</h3>
-      <p>
-        Peter Bjorn & John, Existensminimum, Lisa Ekdahl, EP’s Trailerpark,
-        Soles of men, San Fairy Ann, APA
-      </p>
+      <section className={styles.film}>
+        <figure className={styles['profile-pic']}>
+          <Image
+            src={bror}
+            alt="Poster for the film."
+            placeholder="blur"
+            fill
+          />
+        </figure>
+        <div className={styles['film-desc']}>
+          <ul>
+            <li>
+              <span>Director</span> Isabella Carbonell
+            </li>
+            <li>
+              <span>Producer</span> Alegren Productions
+            </li>
+            <li>
+              <span>15 min</span>
+            </li>
+          </ul>
+          <div>
+            <p>
+              <i>
+                Nico and Khalid are best friends, always have been. Khalid was
+                born in Iran, a country where having feelings for another man
+                can lead to the death penalty. Nico was born in Sweden, a
+                country where it's openly accepted for a man to have a
+                boyfriend. And yet it's Nico who keeps holding back when Khalid
+                tries to take their relationship to another level.
+              </i>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   </Layout>
 );
--- pages/dop/yustina.tsx
+++ pages/dop/yustina.tsx
@@ -1,24 +1,63 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@styles/Flick.module.scss';
 import Layout from '@components/layout/FlickLayout';
+import yustina from 'assets/images/flick/projects/yustina-poster.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Yustina = () => (
   <Layout>
     <main className={styles.flick}>
       <h2>Yustinas Hämnd</h2>
-      <h3>Education</h3>
-      <ul>
-        <li>
-          2012-2015 Bachelor in cinematography at Stockholm academy of dramatic
-          arts, Sweden
-        </li>
-        <li>2006-2008 Basic filmproduction at Stockholm filmskola, Sweden</li>
-      </ul>
-      <h3>Music promos</h3>
-      <p>
-        Peter Bjorn & John, Existensminimum, Lisa Ekdahl, EP’s Trailerpark,
-        Soles of men, San Fairy Ann, APA
-      </p>
+      <section className={styles.film}>
+        <figure className={styles['profile-pic']}>
+          <Image
+            src={yustina}
+            alt="Poster for the film."
+            placeholder="blur"
+            fill
+          />
+        </figure>
+        <div className={styles['film-desc']}>
+          <ul>
+            <li>
+              <span>Director</span> Isabella Carbonell
+            </li>
+            <li>
+              <span>Producer</span> Madbunny Film
+            </li>
+            <li>
+              <span>15 min</span>
+            </li>
+          </ul>
+          <div>
+            <p>
+              <i>
+                Yustina’s sister is missing. She left their village in Kosovo to
+                find work for them in Sweden, but the letters stopped coming.
+                Now Yustina finds herself in this new country where she, all by
+                herself, has to find and save her big sister.
+              </i>
+            </p>
+            <p>
+              Yustina’s journey will be filled with danger, so in order to cope
+              with a brutal reality, she uses her imagination to create her own
+              world. A world where the people that has imprisoned her sister are
+              monsters that only she can defeat.
+            </p>
+            <p className={styles.extra}>
+              –{' '}
+              <a
+                className={styles.link}
+                href="https://drakenfilm.se/film/yustinas-hamnd"
+              >
+                Watch the film on Draken Film{' '}
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   </Layout>
 );
