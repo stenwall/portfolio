3431d719b33777ce59119ae0dd6774ea5f3fbb7b
Karin <karin.stenwall@gmail.com>
2023-02-24 20:33:11 +0100

    chore: code clean up

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -18,18 +18,3 @@ const useMediaQuery = (query: string) => {
 };
 
 export default useMediaQuery;
-
-// function useMediaQuery(query) {
-//   const [matches, setMatches] = React.useState(false);
-//   React.useEffect(() => {
-//     const matchQueryList = window.matchMedia(query);
-//     function handleChange(e) {
-//       setMatches(e.matches);
-//     }
-//     matchQueryList.addEventListener("change", handleChange);
-//     return () => {
-//       matchQueryList.removeEventListener("change", handleChange);
-//     };
-//   }, [query]);
-//   return matches;
-// }
